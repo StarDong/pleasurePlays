20cc61e23198ebefb55d3dccd22f1f8e9d275553
qinsw <qinsw@ifeng.com>
2019-04-16 17:39:18 +0800

    安装less

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+     <div class="main">2222</div>
     <router-view/>
   </div>
 </template>
@@ -11,7 +12,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +20,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .main{
+    font-size: 60px;
+  }
 }
+
 </style>
